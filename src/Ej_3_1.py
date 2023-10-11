5b42e188252555d84b99e0d30228a670222e4fed
Ejercicios_DAW_Python <aarrieta@iesrafaelalberti.es>
2023-10-11 19:31:15 +0200

    Configuración Inicial

---
+++ src/Ej_3_1.py
@@ -0,0 +1,7 @@
+def ____():
+    return ____
+
+
+if __name__ == "__main__":
+    default = str(input(">:  "))
+    print(____(default))
---
+++ test/test_3_1.py
@@ -0,0 +1,5 @@
+from src.A_Default import ____
+
+
+def test_____():
+    assert ____("Hola") == 00
